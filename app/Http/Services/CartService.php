917b3ad02da44752e66d4fbd692d5480e37c79a5
Huy Quoc Dang <85658305+danieldev23@users.noreply.github.com>
2022-06-08 15:49:56 +0700

    Update CartService.php

--- app/Http/Services/CartService.php
+++ app/Http/Services/CartService.php
@@ -102,11 +102,12 @@ class CartService
             SendMail::dispatch($request->input('email'))->delay(now()->addSeconds(2));
 
             Session::forget('carts');
-        } catch (\Exception $err) {
-            DB::rollBack();
-            Session::flash('error', 'Đặt Hàng Lỗi, Vui lòng thử lại sau');
-            return false;
-        }
+        } 
+//         catch (\Exception $err) {
+//             DB::rollBack();
+//             Session::flash('error', 'Đặt Hàng Lỗi, Vui lòng thử lại sau');
+//             return false;
+//         }
 
         return true;
     }
