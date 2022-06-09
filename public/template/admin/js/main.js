ab9ddef121f4341df2de39e503490348b8d37690
Huy Quoc Dang <85658305+danieldev23@users.noreply.github.com>
2022-06-09 20:41:41 +0700

    Update main.js

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -1,12 +1,6 @@
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
 });
 function removeRow(id, url) {
