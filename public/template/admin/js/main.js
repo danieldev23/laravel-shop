bb2e4fb487e162524e6e816e095a4dbf39a31bf7
Huy Quoc Dang <85658305+danieldev23@users.noreply.github.com>
2022-06-09 20:16:18 +0700

    Update main.js

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -1,9 +1,14 @@
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
 $.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
     }
 });
-
 function removeRow(id, url) {
     if (confirm('Xóa mà không thể khôi phục. Bạn có chắc ?')) {
         $.ajax({
