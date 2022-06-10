fab8019310e51ee4d5c7fec82689a91b1619a507
Huy Quoc Dang <85658305+danieldev23@users.noreply.github.com>
2022-06-10 10:21:53 +0700

    Update main.js

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -10,46 +10,48 @@ $.ajaxSetup({
 });
 
 function removeRow(id, url) {
-    if (confirm('Xóa mà không thể khôi phục. Bạn có chắc ?')) {
-        $.ajax({
-            type: 'DELETE',
-            datatype: 'JSON',
-            data: { id },
-            url: url,
-            success: function (result) {
-                if (result.error === false) {
-                    alert(result.message);
-                    location.reload();
-                } else {
-                    alert('Xóa lỗi vui lòng thử lại');
-                }
-            }
-        })
-    }
-}
-
-
-/*Upload File */
-$('#upload').change(function () {
-    const form = new FormData();
-    form.append('file', $(this)[0].files[0]);
-
+  if (confirm("Xóa mà không thể khôi phục. Bạn có chắc ?")) {
     $.ajax({
-        processData: false,
-        contentType: false,
-        type: 'POST',
-        dataType: 'JSON',
-        data: form,
-        url: '/admin/upload/services',
-        success: function (results) {
-            if (results.error === false) {
-                $('#image_show').html('<a href="' + results.url + '" target="_blank">' +
-                    '<img src="' + results.url + '" width="100px"></a>');
-
-                $('#thumb').val(results.url);
-            } else {
-                alert('Upload File Lỗi');
-            }
+      type: "DELETE",
+      datatype: "JSON",
+      data: { id },
+      url: url,
+      success: function (result) {
+        if (result.error === false) {
+          alert(result.message);
+          location.reload();
+        } else {
+          alert("Xoá thành công");
         }
+      },
     });
+  }
+}
+/*Upload File */
+$("#upload").change(function () {
+  const form = new FormData();
+  form.append("file", $(this)[0].files[0]);
+  $.ajax({
+    processData: false,
+    contentType: false,
+    type: "POST",
+    dataType: "JSON",
+    data: form,
+    url: "/admin/upload/services",
+    success: function (results) {
+      if (results.error === false) {
+        $("#image_show").html(
+          '<a href="' +
+            results.url +
+            '" target="_blank">' +
+            '<img src="' +
+            results.url +
+            '" width="100px"></a>'
+        );
+        $("#thumb").val(results.url);
+      } else {
+        alert("Upload File Thành Công");
+      }
+    },
+  });
 });
