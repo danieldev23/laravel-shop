19ed63bb3e82a79910d6c0182a4ba97178ba1df3
Huy Quoc Dang <85658305+danieldev23@users.noreply.github.com>
2022-06-10 13:28:58 +0700

    Update head.blade.php

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
 <base href="{{ asset('/') }}">
-<link rel="icon" type="image/png" href="{{asset('template/images/icons/favicon1.png')}}"/>
+<link rel="icon" type="image/png" href="{{asset('template/images/icons/f.png')}}"/>
 <!--=====================================/==========================================================-->
 <link rel="stylesheet" type="text/css" href="{{asset('template/vendor/bootstrap/css/bootstrap.min.css')}}">
 <!--===============================================================================================-->
