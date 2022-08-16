1e41f7356ed8746ebba25ab9bfe3b420405fd69d
Huy Quoc Dang <85658305+danieldev23@users.noreply.github.com>
2022-08-16 22:31:39 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,22 +12,22 @@
                         <div class="container h-full">
                             <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                                 <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
-                            <span class="ltext-101 cl2 respon2">
-                                HOT 2021
-                            </span>
+                    <!--        <span class="ltext-101 cl2 respon2">
+                                HOT 2022
+                            </span> -->
                                 </div>
 
                                 <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
-                                    <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
+                       <!--             <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
                                         {{ $slider->name }}
-                                    </h2>
+                                    </h2> -->
                                 </div>
 
-                                <div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
+                           <!--     <div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
                                     <a href="{{ $slider->url }}" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
                                         Shop Now
                                     </a>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </div>
@@ -43,7 +43,7 @@
                     <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
                         <!-- Block1 -->
                         <div class="block1 wrap-pic-w">
-                            <img src="/template/images/banner-01.jpg" alt="IMG-BANNER">
+                            <img src="/template/images/banner-10.jpg" alt="IMG-BANNER">
 
                             <a href="/danh-muc/{{ $menu->id }}-{{ \Str::slug($menu->name, '-') }}.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                                 <div class="block1-txt-child1 flex-col-l">
@@ -52,7 +52,7 @@
                             </span>
 
                                     <span class="block1-info stext-102 trans-04">
-                                HOT 2021
+                                HOT 2022
                             </span>
                                 </div>
 
@@ -85,7 +85,7 @@
 				</div>
 				<!-- Filter -->
 			</div>
-			{{-- <div class="row isotope-grid"> --}}
+	<div id="loadProduct">
 				@include('products.list')
 </div>
 <div class="flex-c-m flex-w w-full p-t-45" id="button-loadMore">
